2d7dcf304476a0b65f4112fe51334112a21d8dd1
Keily Salinas <keisalinas2@gmail.com>
2018-08-08 22:09:12 -0500

    added suggestion list component

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
 
 import Home from './App/Screens/Containers/Home';
 import Header from './App/Sections/Components/Header';
+import SuggestionList from './App/Videos/Containers/SuggestionList';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -16,6 +17,7 @@ export default class App extends Component<Props> {
         <Text>buscador</Text>
         <Text>categorías</Text>
         <Text>sugerencias</Text>
+        <SuggestionList />
       </Home>
     );
   }
---
+++ App/Videos/Containers/SuggestionList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  FlatList,
+  Text
+} from 'react-native';
+
+class SuggestionList extends Component {
+  render() {
+    const list = [
+      {
+        title: 'Kei',
+        key: '1'
+      },
+      {
+        title: 'Salinas',
+        key: '2'
+      }
+    ]
+    return (
+      <FlatList
+        data={list}
+        renderItem={({ item }) => <Text>{item.title}</Text> }
+      />
+    )
+  }
+}
+
+export default SuggestionList
