4a1eb6b33060ab8b7f3f123c2cee9793f20b538d
Keily Salinas <keisalinas2@gmail.com>
2018-08-08 20:14:58 -0500

    added header component

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import {
 } from 'react-native';
 
 import Home from './App/Screens/Containers/Home';
+import Header from './App/Sections/Components/Header';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <Home>
+        <Header />
         <Text>header</Text>
         <Text>buscador</Text>
         <Text>categorías</Text>
