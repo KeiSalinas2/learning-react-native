f2903863d06b478afc93a87121b2eeb5774b82fb
Keily Salinas <keisalinas2@gmail.com>
2018-08-11 00:20:46 -0500

    show movies list

--- App.js
+++ App.js
@@ -6,18 +6,30 @@ import {
 import Home from './App/Screens/Containers/Home';
 import Header from './App/Sections/Components/Header';
 import SuggestionList from './App/Videos/Containers/SuggestionList';
+import API from './App/Utils/Api/Api';
 
 type Props = {};
+type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    suggestionList: [],
+  }
+  async componentDidMount() {
+    const movies = await API.getSuggestion(10);
+    console.log(movies);
+    this.setState({
+      suggestionList: movies,
+    })
+  }
   render() {
     return (
       <Home>
         <Header />
-        <Text>header</Text>
         <Text>buscador</Text>
         <Text>categorías</Text>
-        <Text>sugerencias</Text>
-        <SuggestionList />
+        <SuggestionList
+          list={this.state.suggestionList}
+        />
       </Home>
     );
   }
